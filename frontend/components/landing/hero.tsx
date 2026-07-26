2f1f10536509e0638be906f879209c0c503d6c7e
Mohit Singh <mohittjee@gmail.com>
2026-07-26 17:12:06 +0530

    refactor(frontend): yt link

--- frontend/components/landing/hero.tsx
+++ frontend/components/landing/hero.tsx
@@ -46,14 +46,14 @@ export function LandingHero() {
                         className="size-3.5"
                       />
                     </Button>
-                    {/* <Button
+                    <Button
                       type="button"
                       size="lg"
                       variant="outline"
                       className="pl-5"
                       onClick={() =>
                         window.open(
-                          "https://youtu.be/Fy5Hk0adtsw",
+                          "https://youtu.be/zXBpUpaiY4k",
                           "_blank",
                           "noopener,noreferrer"
                         )
@@ -61,7 +61,7 @@ export function LandingHero() {
                     >
                       <HugeiconsIcon icon={Play} strokeWidth={2} className="size-3.5" />
                       <span className="text-nowrap">View demo</span>
-                    </Button> */}
+                    </Button>
                   </div>
 
                   <LogoCloud className="mt-10" />
